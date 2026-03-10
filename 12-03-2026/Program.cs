c9a617eb0d6e94ad132ae86b171d45e1e5f54c09
MannavaKamal <2200032973@kluniversity.in>
2026-03-10 20:10:38 +0530

    10-02-2026

---
+++ 12-03-2026/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _12_03_2026;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        if (Enum.TryParse(Console.ReadLine(), out Planet planet))
+        {
+            Console.WriteLine(GetGravity(planet));
+        }
+        else
+        {
+            Console.WriteLine("Enter the valid Planet");
+        }
+    }
+
+    static double GetGravity(Planet planet)
+    {
+        switch (planet)
+        {
+            case Planet.MERCURY: return 0.38;
+            case Planet.VENUS: return 0.91;
+            case Planet.EARTH: return 1.0;
+            case Planet.MARS: return 0.38;
+            case Planet.JUPITER: return 2.34;
+            case Planet.SATURN: return 1.06;
+            case Planet.URANUS: return 0.92;
+            case Planet.NEPTUNE: return 1.19;
+            default: return 1.0;
+        }
+    }
+}
+
+enum Planet
+{
+    MERCURY,
+    VENUS,
+    EARTH,
+    MARS,
+    JUPITER,
+    SATURN,
+    URANUS,
+    NEPTUNE
+}

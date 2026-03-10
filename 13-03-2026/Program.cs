aa4d2e6d7614c61d9c53230280253cf9090268a7
MannavaKamal <2200032973@kluniversity.in>
2026-03-10 19:19:08 +0530

    10-03-2026

---
+++ 13-03-2026/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _13_03_2026
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("kamal", "20261",new double[5] {100,99,85,90,97});
+            double averageMarks = student.CalculateAverage();
+            Console.WriteLine($"averageMarks = {averageMarks}");
+            student.CalculateGrade(averageMarks);
+        }
+    }
+}
---
+++ 13-03-2026/Student.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _13_03_2026;
+
+public class Student
+{
+    public string Name;
+    public string RollNumber;
+    public double[] Marks ;
+
+    public Student(string Name, string RollNumber, double[] Marks)
+    {
+        this.Name = Name;
+        this.RollNumber = RollNumber;
+        this.Marks = Marks;
+    }
+
+    public double CalculateAverage()
+    {
+        double sum = 0.0;
+        foreach (double mark in Marks)
+        {
+            sum = sum + mark;
+        }
+        return sum / 5;
+    }
+
+    public void CalculateGrade(double AverageMarks)
+    {
+        if (AverageMarks >= 90 && AverageMarks <= 100)
+        {
+            Console.Write("A+");
+        }
+        else if (AverageMarks >= 80)
+        {
+            Console.Write("A");
+        }
+        else if (AverageMarks >= 70)
+        {
+            Console.Write("B");
+        }
+        else if (AverageMarks >= 60)
+        {
+            Console.Write("C");
+        }
+        else if (AverageMarks >= 50)
+        {
+            Console.Write("D");
+        }
+        else
+        {
+            Console.Write("Fail");
+        }
+
+        Console.Write(" Grade");
+
+
+
+    }
+}
+
